26d858ca2e8197c8d3415443441fde6dfc174f00
Yakub Bello <yakubbello01@gmail.com>
2023-02-21 17:06:41 -0800

    feat: print to 98

---
+++ 0x02-functions_nested_loops/11-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check your code
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
+	return (0);
+}
