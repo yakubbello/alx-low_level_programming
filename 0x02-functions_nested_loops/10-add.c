c56e8bd1940160ec05cb8f86daef22f7599766d8
Yakub Bello <yakubbello01@gmail.com>
2023-02-21 17:03:18 -0800

    feat: addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add- adds two numbers and return the result
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: always return the result of the operation
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check your code
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	c = add(89, 9);
+	printf("%d\n",c);
+	return 0;
+}
